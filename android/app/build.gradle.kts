866f41d3ed8e3f4ecec6c2cdd13db2c2a82403b9
windy66 <boren.lawrence@gmail.com>
2026-07-16 10:52:10 +0800

    safety: API key must be kept hidden

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,6 +7,19 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+import java.util.Properties
+
+// Create a function to load the .env file
+fun getEnvProperty(key: String): String {
+    val envFile = rootProject.file(".env")
+    if (envFile.exists()) {
+        val properties = Properties()
+        properties.load(envFile.inputStream())
+        return properties.getProperty(key) ?: ""
+    }
+    return ""
+}
+
 android {
     namespace = "com.example.geo_mugger"
     compileSdk = flutter.compileSdkVersion
@@ -22,6 +35,15 @@ android {
         applicationId = "com.example.geo_mugger"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
+        
+        val apiKey = getEnvProperty("MY_API_KEY")
+        
+        // Pass it to Java/Kotlin code
+        buildConfigField("String", "MY_API_KEY", "\"$apiKey\"")
+        
+        // Pass it to Manifest (if needed)
+        manifestPlaceholders["apiKey"] = apiKey
+
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
